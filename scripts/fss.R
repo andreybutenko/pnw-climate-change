c15523b9224e565d6f6b98d58b453f065e6f99ba
Andrey Butenko <ghostmancer@gmail.com>
2017-12-05 20:46:13 -0800

    Add  fss suitability chart

--- scripts/fss.R
+++ scripts/fss.R
@@ -5,8 +5,11 @@ library(gridExtra)
 
 source('./scripts/hydro-helper.R')
 
-fss.data <- read.csv('./data/fss-stream-temp/stream-temps-combined.csv', stringsAsFactors = F)
+# fss.data <- read.csv('./data/fss-stream-temp/stream-temps-combined.csv', stringsAsFactors = F)
 stream.temp.oly <- read.csv('./data/fss-stream-temp/stream-temp-oly.csv', stringsAsFactors = F)
+stream.temp.grid <- read.csv('./data/fss-stream-temp/stream-temp-grid.csv', stringsAsFactors = F)
+
+# Olympic Penninsula: Stream Temp Intro ----
 
 future.stream.temp <- stream.temp.oly %>% 
   select(x, y, mwmt.a1b.2070.2099) %>% 
@@ -45,6 +48,73 @@ diff.stream.temp.chart <- diff.stream.temp %>%
     offset.long = 0.05, color.legend.title = 'Temperature (degC)'
   )
 
-# grid.arrange(historic.stream.temp, future.stream.temp, ncol = 2)
+#grid.arrange(historic.stream.temp.chart, future.stream.temp.chart, ncol = 2)
 
 
+
+# Washington State Temps ----
+
+wa.stream.temp.chart <- MapPNWData(
+  stream.temp.grid,
+  color.high = 'red', color.mid = NULL, point.size = stream.temp.grid$value / 5,
+  title = 'Washington State Stream Temperature',
+  subtitle = 'A1B Scenario, Avg 2070-2099'
+)
+
+
+
+# Suitability Data ----
+
+CategorizeTemp <- function(temp) {
+  if(temp < 18) {
+    return('good')
+  } else if (temp >= 18 & temp < 22) {
+    return('stressful')
+  } else {
+    return('deadly')
+  }
+}
+
+suitability.data <- stream.temp.grid
+suitability.data$suitability <- sapply(suitability.data$value, CategorizeTemp)
+
+MapSuitabilityData <- function(df, column = 'suitability', title = 'Chart', subtitle = NULL) {
+  pnw.map <- fortify(map_data('state', region = c('washington')))
+  
+  map.data <- df
+  
+  plot <- ggplot(data = map.data) +
+    geom_map(
+      data = pnw.map,
+      map = pnw.map,
+      mapping = aes(x = long, y = lat, map_id = region),
+      fill = '#ffffff'
+    ) +
+    geom_point(
+      mapping = aes(
+        x = x,
+        y = y,
+        color = map.data[,column]
+      ),
+      size = 4,
+      show.legend = TRUE
+    ) +
+    scale_color_manual(
+      name = 'Suitability',
+      values = c(
+        'good' = 'darkgreen',
+        'stressful' = 'orange',
+        'deadly' = 'red'
+      )
+    ) +
+    ggtitle(title, subtitle = subtitle) +
+    coord_fixed(
+      ratio = 1.3,
+      xlim = c(-125, -116),
+      ylim = c(45.5, 49)
+    )
+  
+  return(plot)
+}
+
+suitability.data.chart <- MapSuitabilityData(suitability.data, title = 'Suitability for Salmon Spawning', subtitle = 'A1B Scenario, Avg 2070-2099')
