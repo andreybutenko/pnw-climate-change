c8326b89eaab0e0500a9801024c0de3d4abd066c
SabrinaHersh <hershs4@uw.edu>
2017-12-05 18:06:01 -0800

    datasets cleaned

--- scripts/GCMprojections-PNW.R
+++ scripts/GCMprojections-PNW.R
@@ -3,8 +3,79 @@ library(dplyr)
 
 avg.change.raw <- read.csv("../data/GCM-avg-change.csv", stringsAsFactors = F)
 time.evolv.raw <- read.csv("../data/GCM-TEP.csv", stringsAsFactors = F)
+# rcp45 =  "In CMIP5, for example, "rcp45" refers to a particular experiment
+# in which a "representative concentration pathway" (RCP) has been specified which leads to an
+# approximate radiative forcing of 4.5 W m-2" 
+# 4.5 W m^-2 is Intensity in terms of "energy transmitted per second over an area of a square meter"
+
+# Finding appropriate model
+te.models <- unique(time.evolv.proj$Model)
+ac.models <- unique(avg.change.proj$`Model Name`)
+models.in.both <- ac.models %in% te.models
+model.names <- ac.models[models.in.both]
+model.chosen <- "GFDL-ESM2G"
 
 # Average Change Projections Clean Up
 avg.change.proj <- avg.change.raw[3:length(avg.change.raw$X),]
 colnames(avg.change.proj) <- avg.change.raw[2,]
+avg.change.proj$Season <- recode(avg.change.proj$Season, 
+                                 "ANN" = "Annual",
+                                 "DJF" = "Winter",
+                                 "MAM" = "Spring",
+                                 "JJA" = "Summer",
+                                 "SON" = "Autumn")
+avg.change.proj$Variable <- recode(avg.change.proj$Variable,
+                                   "tas" = "Near-Surface Air Temperature",
+                                   "pr" = "Precipitation")
+avg.change.proj$Scenario <- recode(avg.change.proj$Scenario,
+                                   "rcp26" = "RCP2.6", 
+                                   "rcp45" = "RCP4.5", 
+                                   "rcp60" = "RCP6", 
+                                   "rcp85" = "RCP8.5")
+avg.change.proj <- avg.change.proj %>% 
+  filter(`Projected Change` != "NaN") %>% 
+  filter(`Model Name` == model.chosen)
 
+# Split average change into two datasets
+avg.change.temp <- avg.change.proj %>% 
+  filter(Variable == "Near-Surface Air Temperature")
+avg.change.prec <- avg.change.proj %>% 
+  filter(Variable == "Precipitation")
+
+# Time Evolving Projections Clean Up
+time.evolv.proj <- time.evolv.raw
+colnames(time.evolv.proj) <- c("Variable", "Epoch", "Scenario", "Season", "Model", "Year", "Value")
+time.evolv.proj$Scenario <- recode(time.evolv.proj$Scenario,
+                                   "rcp26" = "RCP2.6", 
+                                   "rcp45" = "RCP4.5", 
+                                   "rcp60" = "RCP6", 
+                                   "rcp85" = "RCP8.5")
+time.evolv.proj$Variable <- recode(time.evolv.proj$Variable,
+                                   "tas" = "Near-Surface Air Temperature",
+                                   "pr" = "Precipitation")
+time.evolv.proj$Season <- recode(time.evolv.proj$Season, 
+                                 "ANN" = "Annual",
+                                 "DJF" = "Winter",
+                                 "MAM" = "Spring",
+                                 "JJA" = "Summer",
+                                 "SON" = "Autumn")
+time.evolv.proj$Epoch <- recode(time.evolv.proj$Epoch,
+                                "historical" = "Historical",
+                                "future" = "Future")
+time.evolv.proj <- time.evolv.proj %>% 
+  filter(Model == model.chosen) %>% 
+  select(-Model)
+
+# Split time evolving projections into two datasets
+time.evolv.temp <- time.evolv.proj %>% 
+  filter(Variable == "Near-Surface Air Temperature") %>% 
+  select(-Variable) %>% 
+  group_by(Year = trunc((Year/10))*10, Epoch, Season, Scenario) %>% 
+  summarise(mean(Value))
+time.evolv.prec <- time.evolv.proj %>% 
+  filter(Variable == "Precipitation") %>% 
+  select(-Variable) %>% 
+  group_by(Year = trunc((Year/10))*10, Epoch, Season, Scenario) %>% 
+  summarise(mean(Value))
+
+# Create new data Files to use
